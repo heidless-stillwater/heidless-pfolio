c8a248bf9c57802ae598627ec50c7c6bf659864f
heidless <rob.lockhart@yahoo.co.uk>
2022-08-18 17:06:14 +0100

    first

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ DEBUG = config('DEBUG', cast=bool, default=True)
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'heidless-portfolio.herokuapp.com',
+    'https://heidless-portfolio.herokuapp.com',
 ]
 
 # Application definition
