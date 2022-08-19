bd0da7dba0de3a713bd3245b49d8a52a5966d467
heidless <rob.lockhart@yahoo.co.uk>
2022-08-19 13:43:23 +0100

    static test 20

--- config/settings.py
+++ config/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'static')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
