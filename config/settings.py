8757f5927b6c3ab72a771451004137610c6663f6
heidless <rob.lockhart@yahoo.co.uk>
2022-08-19 09:33:22 +0100

    static test 3

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = config('SECRET_KEY')
 
-#DEBUG = config('DEBUG')
+##DEBUG = config('DEBUG') 
 DEBUG = config('DEBUG', cast=bool, default=True)
 
 ALLOWED_HOSTS = [
